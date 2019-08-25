1bb8699210426e8d9b24e34def87051c1a729d91
KuanyshGitHub <kuanysh.tazhibayev@nu.edu.kz>
2019-08-25 21:09:27 +0500

    Initial Commit

---
+++ MeOn_1.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeOn_1.1
+//
+//  Created by Kuanysh on 8/25/19.
+//  Copyright © 2019 Kuanysh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
