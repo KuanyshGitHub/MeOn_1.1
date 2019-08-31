79f0f5d53a6572217d8081a5d92568b5294a43ae
KuanyshGitHub <kuanysh.tazhibayev@nu.edu.kz>
2019-08-31 14:31:28 +0500

    Added delete function

--- MeOn_1.1/ViewController.swift
+++ MeOn_1.1/ViewController.swift
@@ -8,13 +8,186 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Tasks"
+        
+        collectionView.backgroundColor = UIColor.white
+        collectionView.alwaysBounceVertical = true
+        
+        collectionView.register(TasksCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView.register(TasksHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerId")
+        
+        if let x = UserDefaults.standard.object(forKey: "cellKey"){
+            tasks = x as! [String]
+        }
+        
     }
+    
+    var tasks: [String] = ["Get paycheck", "Submit report", "Develop an app"]
+    var new: [String] = [""]
 
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let taskCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! TasksCell
+        taskCell.nameLabel.text = tasks[indexPath.item]
+        return taskCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width,height: 50.0)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header =  collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath) as! TasksHeader
+        header.viewController = self
+        return header
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100.0)
+    }
+    
+    func addNewTask(taskName: String) {
+        tasks.append(taskName)
+        UserDefaults.standard.set(tasks, forKey: "cellKey")
+        collectionView.reloadData()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+        tasks.remove(at: indexPath.row)
+        collectionView.deleteItems(at: [indexPath])
+        UserDefaults.standard.set(tasks, forKey: "cellKey")
+    }
+    
+    
+    
+    
 }
+
+
+class TasksHeader: UICollectionViewCell {
+    var viewController: ViewController?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let taskNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Task Name"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    
+    let addTaskButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("add Task", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    
+    func setupViews(){
+        
+        addSubview(taskNameTextField)
+        addSubview(addTaskButton)
+        
+        addTaskButton.addTarget(self, action: #selector(TasksHeader.addTask), for: .touchUpInside)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-[v1(80)]-8-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": taskNameTextField, "v1": addTaskButton]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-24-[v0]-24-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": taskNameTextField]))
+         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": addTaskButton]))
+        
+    }
+    @objc func addTask(){
+        if taskNameTextField.text != "" {
+            viewController?.addNewTask(taskName: taskNameTextField.text!)
+        }
+        taskNameTextField.text = ""
+    }
+}
+
+class TasksCell: UICollectionViewCell, UIGestureRecognizerDelegate {
+    
+    var pan: UIPanGestureRecognizer!
+    var originalCenter = CGPoint()
+    var deleteOnDragRelease = false
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sample text"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+    
+        return label
+    }()
+    
+    func setupViews(){
+        
+        addSubview(nameLabel)
+
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-8-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        
+        pan = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        pan.delegate = self
+        self.addGestureRecognizer(pan)
+        
+    }
+    
+    @objc func onPan(_ pan: UIPanGestureRecognizer){
+        if pan.state == UIGestureRecognizer.State.began {
+
+        } else if pan.state == UIGestureRecognizer.State.changed {
+            self.setNeedsLayout()
+        } else {
+            if abs(pan.velocity(in: self).x) > 300 {
+                let collectionView: UICollectionView = self.superview as! UICollectionView
+                let indexPath: IndexPath = collectionView.indexPathForItem(at: self.center)!
+                collectionView.delegate?.collectionView!(collectionView, performAction: #selector(onPan(_:)), forItemAt: indexPath, withSender: nil)
+            } else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.setNeedsLayout()
+                    self.layoutIfNeeded()
+                })
+            }
+        }
+        
+
+        
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return abs((pan.velocity(in: pan.view)).x) > abs((pan.velocity(in: pan.view)).y)
+    }
+    
+}
+
 
